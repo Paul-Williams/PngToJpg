1cdb4d00418d0fb5d4abaddc1b0c046722951de0
Paul Williams <p.b.williams@outlook.com>
2021-03-26 13:09:16 +0000

    Add project files.

---
+++ PngToJpg/ImageHelper.cs
@@ -0,0 +1,29 @@
+﻿# nullable enable
+
+using System;
+using System.Drawing.Imaging;
+using System.Linq;
+
+// See: https://stackoverflow.com/questions/1484759/quality-of-a-saved-jpg-in-c-sharp
+
+namespace PngToJpg
+{
+  internal static class ImageHelper
+  {
+    public static ImageCodecInfo? GetImageCodecInfo(ImageFormat format) 
+      => ImageCodecInfo.GetImageDecoders().FirstOrDefault(x => x.FormatID == format.Guid);
+
+    public static ImageCodecInfo? GetJpegCodecInfo() => GetImageCodecInfo(ImageFormat.Jpeg);
+
+
+    public static EncoderParameters GetQualityEncoder(long quality)
+    {
+      if (quality is < 1 or > 100) throw new ArgumentException($"'{nameof(quality)}' must be between 0 and 100 (inclusive).");
+
+      var encoderParams = new EncoderParameters(1);
+      encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, quality);
+      return encoderParams;
+    }
+
+  }
+}
---
+++ PngToJpg/Program.cs
@@ -0,0 +1,67 @@
+﻿#nullable enable
+
+using PW.IO.FileSystemObjects;
+using System;
+using System.Drawing;
+using System.Linq;
+
+namespace PngToJpg
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args.Length == 0)
+      {
+        Console.WriteLine("Command line argument(s) missing. Requires path(s) to PNG file(s).");
+        return;
+      }
+
+      var pngPaths = args.Where(x => x.EndsWith(".png")).ToArray();
+
+      if (pngPaths.Length == 0)
+      {
+        Console.WriteLine("None of the command line arguments is a PNG file path.");
+        return;
+      }
+
+      // Used to save as Jpeg of specific quality.
+      var codecInfo = ImageHelper.GetJpegCodecInfo();
+
+      if (codecInfo is null)
+      {
+        Console.WriteLine("Unable to get Jpeg codec info.");
+        return;
+      }
+
+      // Quality for saved Jpegs
+      var encoderParams = ImageHelper.GetQualityEncoder(90L);
+
+      // File extension to use for saved Jpegs.
+      var jpgExt = (FileExtension)".jpg";
+
+
+      foreach (var pngFile in pngPaths.Select(x => (FilePath)x))
+      {
+        if (pngFile.Exists)
+        {
+          var jpgFile = pngFile.ChangeExtension(jpgExt);
+
+          if (!jpgFile.Exists)
+          {
+            using var pngImg = Image.FromFile(pngFile.Value);
+            pngImg.Save(pngFile.ChangeExtension(jpgExt).Value, codecInfo, encoderParams);
+          }
+          else Console.WriteLine("File already exists: " + jpgFile.Value);
+
+
+        }
+        else Console.WriteLine("File not found: " + pngFile.Value);
+
+      }
+
+
+
+    }
+  }
+}
