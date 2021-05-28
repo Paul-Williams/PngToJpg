2575031f1f4e2b850fe60863d62f3c86379fc8b9
Paul-Williams <p.b.williams@outlook.com>
2021-05-28 14:22:58 +0100

    Enabled convert all PNG in directory

--- PngToJpg/Program.cs
+++ PngToJpg/Program.cs
@@ -2,6 +2,7 @@
 
 using PW.IO.FileSystemObjects;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks.Dataflow;
 
@@ -11,62 +12,47 @@ namespace PngToJpg
   {
     static async System.Threading.Tasks.Task Main(string[] args)
     {
+      // One or more command line arguments are required.
       if (args.Length == 0)
       {
-        Console.WriteLine("Command line argument(s) missing. Requires path(s) to PNG file(s).");
+        Console.WriteLine("Command line argument(s) missing. Requires path(s) to PNG file(s) or a path to a directory containing PNG files.");
         return;
       }
 
-      var pngPaths = args.Where(x => x.EndsWith(".png")).ToArray();
+      // If there is a single command line argument, which points to a valid directory, then convert all the PNG's within that directory.
+      // Otherwise get all the command line arguments that are PNG file paths.
+      var pngPaths = (args.Length == 1 && Directory.Exists(args[0]))
+        ? Directory.GetFiles(args[0], "*.png")
+        : args.Where(x => x.EndsWith(".png")).ToArray();
+
 
       if (pngPaths.Length == 0)
       {
-        Console.WriteLine("None of the command line arguments is a PNG file path.");
+        Console.WriteLine("None of the command line arguments or the supplied path contain a PNG file path.");
         return;
       }
 
-      //// Used to save as Jpeg of specific quality.
-      //var codecInfo = ImageHelper.GetJpegCodecInfo();
-
-      //if (codecInfo is null)
-      //{
-      //  Console.WriteLine("Unable to get Jpeg codec info.");
-      //  return;
-      //}
-
-      //// Quality for saved Jpegs
-      //var encoderParams = ImageHelper.GetQualityEncoder(90L);
-
-      //// File extension to use for saved Jpegs.
+      // File extension to use for saved Jpegs.
       var jpgExt = (FileExtension)".jpg";
 
       var converter = new DataFlowImageConverter(90L);
-
 
       foreach (var pngFile in pngPaths.Select(x => (FilePath)x))
       {
         if (pngFile.Exists)
         {
+          // Re-create the files full path, but with a JPG extension instead of a PNG extension.
           var jpgFile = pngFile.ChangeExtension(jpgExt);
 
-          if (!jpgFile.Exists)
-          {
-            //using var pngImg = Image.FromFile(pngFile.Value);
-            //pngImg.Save(pngFile.ChangeExtension(jpgExt).Value, codecInfo, encoderParams);
-
-            converter.Loader.Post(new(pngFile, jpgFile));
-
-          }
+          // Convert the PNG only if a JPG with the same name does not already exist.
+          if (!jpgFile.Exists) converter.Loader.Post(new(pngFile, jpgFile));
           else Console.WriteLine("File already exists: " + jpgFile.Value);
-
-
         }
         else Console.WriteLine("File not found: " + pngFile.Value);
 
       }
       converter.Loader.Complete();
       await converter.Saver.Completion;
-
 
     }
   }
